5810921294eb05ed1166e564432d28b390477060
louy2 <loganlyf@gmail.com>
2014-03-02 22:20:50 -0500

    Lab 5 basic finished

--- lab5/index.js
+++ lab5/index.js
@@ -1,11 +1,39 @@
 var Twitter = require('ntwitter');
 var fs = require('fs');
 var cred = require('./cred.js');
+var assert = require('assert')
 
+var n;
+try {
+  n = process.argv[2];
+} catch (err) {
+  console.log('Usage: node index.js <number of tweets>');
+  console.log(err);
+  process.exit(1);
+}
+
+var i = 0;
 var t = new Twitter(cred);
+var r = fs.createWriteStream('./tweets.json');
+r.write('[');
 
-twit.stream('statuses/sample', function(stream) {
+t.stream('statuses/sample', function(stream) {
   stream.on('data', function(data) {
-    console.log(data);
-  })
+    r.write(JSON.stringify(data));
+    console.log(i);
+    if (i++ < n) {
+      r.write(',')
+    } else {
+      stream.destroy();
+    }
+  });
+  stream.on('destroy', function(res) {
+    console.log('Destroyed.');
+    console.log(res);
+    r.end(']');
+  });
+  r.on('finish', function() {
+    console.log('Twitter stream fetched.');
+  });
 });
+
