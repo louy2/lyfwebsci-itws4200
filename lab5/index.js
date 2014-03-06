53155c6bb4bb90f07b0534b975c68873d842b260
louy2 <loganlyf@gmail.com>
2014-03-06 14:41:41 -0500

    Add express server

--- lab5/index.js
+++ lab5/index.js
@@ -1,16 +1,29 @@
-var Twitter = require('ntwitter');
-var fs = require('fs');
-var cred = require('./cred.js');
-var assert = require('assert')
+var express = require('express');
 
-var n;
-try {
+var Twitter = require('ntwitter'),
+    fs = require('fs'),
+    assert = require('assert');
+
+var cred = require('./cred.js');
+
+app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+var server = app.listen(4000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
+
+var n = 1000;
+/*try {
   n = process.argv[2];
 } catch (err) {
   console.log('Usage: node index.js <number of tweets>');
   console.log(err);
   process.exit(1);
-}
+}*/
 
 var i = 0;
 var t = new Twitter(cred);
