dfd4fcaebd5e6896e99f8719dc6e52c4d84a4bea
louy2 <loganlyf@gmail.com>
2014-03-27 16:36:36 -0400

    Lab 6 initial commit

---
+++ lab6/cred.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'BPnEDXJ4XXqxF6iAmyDoA',
+  consumer_secret: 'TInoVD5gSOBhev7HUklis9UbpIYxrp5VWY5BGz2id0',
+  access_token_key: '141857613-3uixsysSZ18cf3wJOYBDW7LkZv8u2XcBgKkCG5mK',
+  access_token_secret: 'geLPRA97oZfMgYfhkjpJXnApdnWqNt3U4OQl5GplJUsXq'
+}
---
+++ lab6/index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+
+var Twitter = require('ntwitter'),
+    fs = require('fs'),
+    assert = require('assert');
+
+var cred = require('./cred.js');
+
+function json2csv(json) {
+  data = JSON.parse(json);
+  header = data.getOwnPropertyNames();
+
+}
+
+app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+var server = app.listen(4000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
+
+var n = 1000;
+/*try {
+  n = process.argv[2];
+} catch (err) {
+  console.log('Usage: node index.js <number of tweets>');
+  console.log(err);
+  process.exit(1);
+}*/
+
+var i = 0;
+var t = new Twitter(cred);
+var r = fs.createWriteStream('./tweets.json');
+r.write('[');
+
+t.stream('statuses/sample', function(stream) {
+  stream.on('data', function(data) {
+    r.write(JSON.stringify(data));
+    console.log(i);
+    if (i++ < n) {
+      r.write(',')
+    } else {
+      stream.destroy();
+    }
+  });
+  stream.on('destroy', function(res) {
+    console.log('Destroyed.');
+    console.log(res);
+    r.end(']');
+  });
+  r.on('finish', function() {
+    console.log('Twitter stream fetched.');
+  });
+});
