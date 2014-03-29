e3fa01e181578457f7d35824fbd681c1c1735c5f
louy2 <loganlyf@gmail.com>
2014-03-29 02:55:40 -0400

    Add json2csv convert

--- lab6/index.js
+++ lab6/index.js
@@ -6,52 +6,65 @@ var Twitter = require('ntwitter'),
 
 var cred = require('./cred.js');
 
+function getTwitterStream(n){
+  var i = 0;
+  var t = new Twitter(cred);
+  var r = fs.createWriteStream('./tweets.json');
+  r.write('[');
+
+  t.stream('statuses/sample', function(stream) {
+    stream.on('data', function(data) {
+      r.write(JSON.stringify(data));
+      console.log(i);
+      if (i++ < n) {
+        r.write(',');
+      } else {
+        stream.destroy();
+      }
+    });
+    stream.on('destroy', function(res) {
+      console.log('Destroyed.');
+      console.log(res);
+      r.end(']');
+    });
+    r.on('finish', function() {
+      console.log('Twitter stream fetched.');
+    });
+  });
+}
+
 function json2csv(json) {
   data = JSON.parse(json);
-  header = data.getOwnPropertyNames();
-
+  header = Object.getOwnPropertyNames(data[0]);
+  //header.foreach(function(ele, ind, arr) {
+    //ele = "\"" + ele + "\"";
+  //});
+  header = header.toString();
+  csv = header + "\n";
+  data.foreach(function(ele, ind, arr) {
+    for (var prop in ele) {
+      if (ele.hasOwnProperty(prop)) {
+        buf = ele[prop];
+        if (typeof buf == 'string') {
+          csv += buf;
+          continue;
+        } else if (Array.isArray(buf)) {
+          csv += buf.toString();
+        } else if (typeof buf == 'object') {
+        }
+        csv += ele[prop].toString;
+      }
+    }
+  });
 }
 
 app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  app.use(express.static('view.html'));
 });
 
 var server = app.listen(4000, function() {
   console.log('Listening on port %d', server.address().port);
 });
 
-var n = 1000;
-/*try {
-  n = process.argv[2];
-} catch (err) {
-  console.log('Usage: node index.js <number of tweets>');
-  console.log(err);
-  process.exit(1);
-}*/
-
-var i = 0;
-var t = new Twitter(cred);
-var r = fs.createWriteStream('./tweets.json');
-r.write('[');
-
-t.stream('statuses/sample', function(stream) {
-  stream.on('data', function(data) {
-    r.write(JSON.stringify(data));
-    console.log(i);
-    if (i++ < n) {
-      r.write(',')
-    } else {
-      stream.destroy();
-    }
-  });
-  stream.on('destroy', function(res) {
-    console.log('Destroyed.');
-    console.log(res);
-    r.end(']');
-  });
-  r.on('finish', function() {
-    console.log('Twitter stream fetched.');
-  });
-});
