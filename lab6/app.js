fa126366107dbd4512c8e41d91c59801c56d856b
louy2 <loganlyf@gmail.com>
2014-04-03 12:48:18 -0400

    Finish Lab 6

--- lab6/app.js
+++ lab6/app.js
@@ -1,5 +1,5 @@
-//var express = require('express');
-//var http = require('http');
+var express = require('express');
+var http = require('http');
 
 var Twitter = require('ntwitter'),
     fs = require('fs'),
@@ -34,62 +34,89 @@ function getTwitterStream(n, f){
   });
 }
 
+function str2csv(s) {
+  return '"' + s + '"';
+}
+
+function arr2csv(arr) {
+  var csv = '';
+  arr.forEach(function(u,i,a){
+    if (!isNaN(u)) {
+      csv += u;
+    } else if (typeof u === 'string') {
+      csv += '"' + u + '"';
+    } else if (Array.isArray(u)) {
+      csv += arr2csv(u);
+    } else if (typeof u === 'object') {
+      csv += obj2csv(u);
+    }
+    csv += ',';
+  });
+  return csv.slice(0,-1);
+}
+
 function json2csv(json) {
   var data = JSON.parse(json);
   // make data an array if not
   if (!Array.isArray(data)) { data = [data]; }
-  var header = Object.getOwnPropertyNames(data[0]);
-  var headstr = "";
-  header.forEach(function(ele, ind, arr) {
-    headstr += '"' + ele + '",';
-  });
-  headstr = headstr.slice(0,-1); // rid of last comma
-  var csv = header + "\r\n";
+  var header = '';
+  for (var prop in data[0]) {
+    header += '"' + prop + '",';
+  }
+  header = header.slice(0,-1) + '\r\n';
+  console.log(header);
+  var csv = '';
   data.forEach(function(ele, ind, arr) {
-    csv += obj2csv(ele, csv);
+    csv += obj2csv(ele);
     csv += "\r\n";
   });
   console.log('Conversion successful.');
-  return csv;
+  return header + csv;
 }
 
-function obj2csv(ele, csv) {
+function obj2csv(ele) {
+  var csv = '';
   for (var prop in ele) {
     var buf = ele[prop];
-    csv += '"';
     if (!isNaN(buf)) {
-      csv += buf + ',';
+      csv += buf;
     } else if (typeof buf === 'string') {
       csv += '"' + buf + '"';
     } else if (Array.isArray(buf)) {
-      buf.forEach(function(ele, ind, arr) {
-        csv += '"' + ele + '",';
-      });
-      csv = headstr.slice(0,-1); // rid of last comma
+      csv += arr2csv(buf);
     } else if (typeof buf === 'object') {
       csv += obj2csv(buf, csv);
     }
-    csv += '",';
+    csv += ',';
   }
-  csv = csv.slice(0,-1); // rid of last comma
-  return csv;
+  return csv.slice(0,-1);
 }
 
-//getTwitterStream(10, './testt.json');
 
-fs.readFile('testt.json', function(err, data) {
-  if (err) throw err;
-  json2csv(data);
-  //fs.writeFile('test.csv',json2csv(data),function(err){
-    //if (err) throw err;
-    //console.log('Conversion saved.');
-  //});
+app = express();
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/getTweets', function(req, res) {
+  if (isNaN(req.query.n)) {
+    throw new Error("Query is not a number.");
+  } else {
+    getTwitterStream(req.query.n,'testt.json');
+  }
+  res.redirect('/');
 });
 
-//app = express();
+app.get('/tweets.csv', function(req, res) {
+  fs.readFile('testt.json', function(err, data) {
+    if (err) throw err;
+    fs.writeFile('test.csv',json2csv(data),function(err){
+      if (err) throw err;
+      console.log('Conversion saved.');
+    });
+  });
+  res.download('test.csv','test.csv');
+});
 
-//app.use(express.static(__dirname + '/'));
-
-//http.createServer(app).listen(80, function() {
-  //console.log('Listening on port %d', this.address().port);
-//});
+http.createServer(app).listen(80, function() {
+  console.log('Listening on port %d', this.address().port);
+});
